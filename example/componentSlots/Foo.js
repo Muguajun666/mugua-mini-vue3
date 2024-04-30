f9d6d6ab9e152da24b06d5f699237f62dcd35d77
Muguajun666 <liushulialen@qq.com>
2024-04-30 14:54:53 +0800

    feature：实现slot

---
+++ example/componentSlots/Foo.js
@@ -0,0 +1,22 @@
+import { h, renderSlots } from '../../lib/mugua-mini-vue3.esm.js';
+
+export const Foo = {
+  setup() {
+    return {};
+  },
+  render() {
+    const foo = h('p', {}, 'foo');
+    // Foo.vnode.children
+    console.log(this.$slots);
+
+    // renderSlots
+    // 具名插槽
+    // 作用域插槽
+    const age = 18;
+    return h('div', {}, [
+      renderSlots(this.$slots, 'header', { age }),
+      foo,
+      renderSlots(this.$slots, 'footer'),
+    ]);
+  },
+};
